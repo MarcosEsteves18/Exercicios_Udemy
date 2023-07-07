922e4106f0f9f6feadff3d24fe167bed932a8981
MarcosEsteves18 <vi_ni_esteves18@hotmail.com>
2023-07-07 17:07:56 -0300

    Dados Pessoas

--- Exercicio_vetor_dados_pessoas/src/Main.java
+++ Exercicio_vetor_dados_pessoas/src/Main.java
@@ -11,18 +11,23 @@ public class Main {
 		System.out.print("Quantos elementos vai ter o vetor? ");
 		n = sc.nextInt();
 		
-		String[] genero = new String[n];
+		char[] genero = new char[n];
 		double[] altura = new double[n];
 		
 		for(int i = 0; i < n; i++) {
-			System.out.println("Altura da "+(i+1)+"a pessoa: ");
-			altura[i] = sc.nextInt();
-			System.out.println("Genero da "+(i+1)+"a pessoa: ");
-			genero[i] = sc.next();
+			System.out.print("Altura da "+(i+1)+"a pessoa: ");
+			altura[i] = sc.nextDouble();
+			System.out.print("Genero da "+(i+1)+"a pessoa: ");
+			genero[i] = sc.next().charAt(0);
 		}
 		
 		double menor = 10000;
+		double media = 0;
+		double mediaF = 0;
 		double maior = 0;
+		int contM = 0;
+		int contF = 0;
+		
 		for(int i = 0; i < n; i++) {
 			if(altura[i] < menor) {
 				menor = altura[i];
@@ -33,7 +38,24 @@ public class Main {
 			
 		}
 		
-
+		for(int i = 0; i < n; i++) {
+			if(genero[i] == 'M') {
+				contM++;
+			}else{
+				media = media + altura[i] ;
+				contF++;
+			}
+			
+		}
+		
+		mediaF = media / contF;
+		
+		System.out.println("Menor altura = " + menor);
+		System.out.println("Maior altura = " + maior);
+		System.out.println("Media das alturas das mulheres = " + String.format("%.2f", mediaF));
+		System.out.println("Numero de homens = " + contM);
+		
+		sc.close();
 	}
 
 }
